0d5719360738343346ec4be8d45b120a2b325be9
sohap-naotp <166151297+sohap-naotp@users.noreply.github.com>
2024-06-05 00:48:14 +0700

    Add files via upload

--- my_socket9.py
+++ my_socket9.py
@@ -6,6 +6,7 @@ import pyttsx3
 import RPi.GPIO as GPIO
 import time
 import threading
+from datetime import datetime
 
 BUTTON_SEND_PIN = 17
 BUTTON_SWITCH_PIN = 27
@@ -24,6 +25,7 @@ engine.setProperty('voice', 'vietnam+f1')
 camera = cv2.VideoCapture(0)
 
 def send_detection_command():
+    print('Start', datetime.now())
     try:
         ret, frame = camera.read()
         if not ret:
@@ -31,14 +33,19 @@ def send_detection_command():
             return
 
         frame = cv2.resize(frame, (640, 480))
+        #cv2.imshow('Client Frame', frame)
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+        #    return
 
         data = pickle.dumps(frame)
         message_size = struct.pack("Q", len(data))
+        
+        print('Send', datetime.now())
 
         client_socket.sendall(b'1' + message_size + data)
 
         response_text = client_socket.recv(1024).decode('utf-8')
-        print(f"Server response: {response_text}")
+        print(f"Server response: {response_text}", datetime.now())
 
         engine.say(response_text)
         engine.runAndWait()
@@ -56,6 +63,8 @@ def switch_model():
 def monitor_buttons():
     try:
         while True:
+            #send_detection_command()
+            #time.sleep(3)
             if GPIO.input(BUTTON_SEND_PIN) == GPIO.LOW:
                 print("Send button pressed, sending detection command to server...")
                 send_detection_command()
@@ -70,8 +79,22 @@ def monitor_buttons():
         GPIO.cleanup()
         camera.release()
         client_socket.close()
+def read_cam():
+    while True:
+        ret, frame = camera.read()
+        if not ret:
+            print("Khng th? d?c frame t? camera.")
+            return
 
+        frame = cv2.resize(frame, (640, 480))
+        cv2.imshow('Client Frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+	
 monitor_buttons_thread = threading.Thread(target=monitor_buttons)
+read_cam_thread = threading.Thread(target=read_cam)
 monitor_buttons_thread.start()
+read_cam_thread.start()
 
 monitor_buttons_thread.join()
+read_cam_thread.join()
